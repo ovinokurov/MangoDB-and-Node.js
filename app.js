977f60a0cc14a09a056cec80a0d6b9da296a9889
Oleg Vinokurov <olegvinokurov@hotmail.com>
2013-09-18 02:21:54 -0700

    Read data from Mongo DB and return Json object http://127.0.0.1:1337/male http://127.0.0.1:1337/female

---
+++ app.js
@@ -0,0 +1,66 @@
+
+var databaseUrl = "localhost"; // "username:password@example.com/mydb"
+var collections = ["users", "reports"]
+var db = require("mongojs").connect(databaseUrl, collections);
+
+/*
+db.users.find({sex: "female"}, function(err, users) {
+    if( err || !users) console.log("No female users found");
+    else users.forEach( function(femaleUser) {
+        console.log(femaleUser);
+    } );
+});
+
+db.users.save({email: "srirangan@gmail.com", password: "iLoveMongo", sex: "male"}, function(err, saved) {
+    if( err || !saved ) console.log("User not saved");
+    else console.log("User saved");
+});
+db.users.save({email: "1srirangan@gmail.com", password: "iLoveMongo", sex: "female"}, function(err, saved) {
+    if( err || !saved ) console.log("User not saved");
+    else console.log("User saved");
+});
+db.users.save({email: "2srirangan@gmail.com", password: "iLoveMongo", sex: "female"}, function(err, saved) {
+    if( err || !saved ) console.log("User not saved");
+    else console.log("User saved");
+});
+db.users.save({email: "3srirangan@gmail.com", password: "iLoveMongo", sex: "female"}, function(err, saved) {
+    if( err || !saved ) console.log("User not saved");
+    else console.log("User saved");
+});
+*/
+
+ /*
+db.users.update({email: "srirangan@gmail.com"}, {$set: {sex: "female"}}, function(err, updated) {
+    if( err || !updated ) console.log("User not updated");
+    else console.log("User updated");
+});
+*/
+
+
+var http = require('http');
+var url = require('url');
+
+http.createServer(function (req, res) {
+    //"female"
+    var sex = url.parse(req.url).pathname.substr(1);
+    console.log(sex);
+    res.writeHead(200, {'Content-Type': 'application/json'})
+    db.users.find({sex: sex}, function(err, users) {
+        //console.log(err +"/n");
+        //console.log(users +"/n");
+        if(err || users=="")
+            //console.log("No " + sex + " users found");
+            //res.write(JSON.stringify([{"Error:" : "No female users found"}]);
+            res.write(JSON.stringify([{"Error:" : "No female users found"}]));
+            //console.log("No users found");
+        else
+            users.forEach( function(femaleUser) {
+            //console.log(femaleUser);
+            res.write(JSON.stringify(femaleUser));
+        } );
+        res.end();
+    });
+
+
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
