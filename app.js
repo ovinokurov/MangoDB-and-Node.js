4c2d640720a83b76e5f0591af4e3329276cba785
Oleg Vinokurov <olegvinokurov@hotmail.com>
2013-09-18 02:29:34 -0700

    search on!

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var databaseUrl = "localhost"; // "username:password@example.com/mydb"
 var collections = ["users", "reports"]
 var db = require("mongojs").connect(databaseUrl, collections);
 
-/*
+
 db.users.find({sex: "female"}, function(err, users) {
     if( err || !users) console.log("No female users found");
     else users.forEach( function(femaleUser) {
@@ -11,6 +11,7 @@ db.users.find({sex: "female"}, function(err, users) {
     } );
 });
 
+ /*
 db.users.save({email: "srirangan@gmail.com", password: "iLoveMongo", sex: "male"}, function(err, saved) {
     if( err || !saved ) console.log("User not saved");
     else console.log("User saved");
